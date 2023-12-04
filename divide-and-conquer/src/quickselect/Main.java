3c443e207462fd7183861806638a94d4f5c90d04
barbourja <s2053424@ed.ac.uk>
2023-12-04 20:49:37 +0000

    Implemented Threaded for QuickSelect

--- divide-and-conquer/src/quickselect/Main.java
+++ divide-and-conquer/src/quickselect/Main.java
@@ -1,39 +1,52 @@
 package quickselect;
 
+import java.util.Arrays;
 import java.util.Random;
 
 public class Main {
 
     public static void main(String[] args) {
-        int n = 100000000;
+        int n = 1000000;
         int k = 1;
         Integer[] arr1 = new Integer[n];
         Integer[] arr2 = new Integer[n];
+        Integer[] arr3 = new Integer[n];
+        Integer[] arr4 = new Integer[n];
         Random rand = new Random();
         for (int i = 0; i < n; i++) {
-            Integer val = rand.nextInt(n * 2);
+            Integer val = rand.nextInt(n);
             arr1[i] = val;
             arr2[i] = val;
+            arr3[i] = val;
+            arr4[i] = val;
         }
         long startTime, timeTaken;
 
         QuickSelectStrategy<Integer> sequential = new Sequential<>();
-        QuickSelectStrategy<Integer> forkJoin = new ForkJoin<>(n/32, 16, sequential);
+        QuickSelectStrategy<Integer> forkJoin = new ForkJoin<>(n/16, 16, sequential);
+        QuickSelectStrategy<Integer> threaded = new Threaded<>(n/16, 1, sequential);
 
         System.out.println("Running sequential...");
         startTime = System.nanoTime();
-        sequential.execute(arr1, k);
+        Integer result_seq = sequential.execute(arr1, k);
         timeTaken = System.nanoTime() - startTime;
-        System.out.println("Sequential: " + (timeTaken/1000000) + " ms");
+        System.out.println((timeTaken/1000000) + " ms");
 
         System.out.println("Running fork/join...");
         startTime = System.nanoTime();
-        forkJoin.execute(arr2, k);
+        Integer result_forkjoin = forkJoin.execute(arr2, k);
         timeTaken = System.nanoTime() - startTime;
-        System.out.println("Sequential: " + (timeTaken/1000000) + " ms");
+        System.out.println((timeTaken/1000000) + " ms");
 
+        System.out.println("Running threaded...");
+        startTime = System.nanoTime();
+        Integer result_threaded = threaded.execute(arr3, k);
+        timeTaken = System.nanoTime() - startTime;
+        System.out.println((timeTaken/1000000) + " ms");
 
-
-
+        Arrays.sort(arr4);
+        if (!arr4[k].equals(result_seq) || !result_seq.equals(result_forkjoin) || !result_forkjoin.equals(result_threaded)) {
+            throw new RuntimeException("Incorrect results!");
+        }
     }
 }
