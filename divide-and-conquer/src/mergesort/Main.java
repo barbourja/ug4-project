848dd930ccc5d00dcc02d02de75e9cea82d2f6a6
barbourja <s2053424@ed.ac.uk>
2024-01-09 20:13:02 +0000

    Enforced strict minimum of 1 for MINIMUM_ARRAY_SIZE

--- divide-and-conquer/src/mergesort/Main.java
+++ divide-and-conquer/src/mergesort/Main.java
@@ -6,7 +6,7 @@ import java.util.Random;
 public class Main {
     public static void main(String[] args) {
         Random rand = new Random();
-        int numElems = 1000000;
+        int numElems = 7;
         Integer[] testArr1 = new Integer[numElems];
         for (int i = 0; i < numElems; i++) {
             testArr1[i] = rand.nextInt(numElems/2);
