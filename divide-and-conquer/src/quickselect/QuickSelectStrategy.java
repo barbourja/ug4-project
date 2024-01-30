0db2546e3216008e7203a9b938237bc96adf2e48
barbourja <s2053424@ed.ac.uk>
2024-01-30 16:40:45 +0000

    Instrument Sequential (QuickSelect) and fix implementation

--- divide-and-conquer/src/quickselect/QuickSelectStrategy.java
+++ divide-and-conquer/src/quickselect/QuickSelectStrategy.java
@@ -2,5 +2,16 @@ package quickselect;
 
 public interface QuickSelectStrategy<T extends Comparable<T>> {
     T execute(T[] searchArr, int k);
-    T execute(T[] searchArr, int start, int end, int k);
+
+    T execute(T[] arr, int start, int end, int k);
+
+    int getMinSize();
+
+    int getParallelism();
+
+    void setMinSize(int size);
+
+    void setParallelism(int parallelism);
+
+    String toString(boolean minSize, boolean parallelism);
 }
--- divide-and-conquer/src/quickselect/Sequential.java
+++ divide-and-conquer/src/quickselect/Sequential.java
@@ -1,10 +1,14 @@
 package quickselect;
 
+import java.util.Arrays;
+
 import static quickselect.Utils.random_partition;
 
 public class Sequential<T extends Comparable<T>> implements QuickSelectStrategy<T>{
 
-    public Sequential() {}
+    protected final int MIN_SEQUENCE_SIZE = 1;
+
+    public Sequential() {} //TODO: fix this algorithm
 
     @Override
     public T execute(T[] searchArr, int k) {
@@ -12,22 +16,59 @@ public class Sequential<T extends Comparable<T>> implements QuickSelectStrategy<
     }
 
     @Override
-    public T execute(T[] arr, int start, int end, int k) { // k in interval [0, end - start]
-        if (k < 0 || k > end - start) {
+    public T execute(T[] arr, int start, int end, int k) { // k in interval [0, (end - start)] i.e doesn't exceed the index bounds of the subarray slice
+        if (k < 0 || k > (end - start)) {
+            System.out.println(k);
             throw new RuntimeException("Invalid value of k!");
         }
-
-        k = start + k;
-
         int pivotIndex = random_partition(arr, start, end);
-        if (pivotIndex == k) {
-            return arr[k];
+        int adjustedPivotIndex = pivotIndex - start; // normalize value to treat subarray start index as 0
+
+        if (adjustedPivotIndex == k) {
+            return arr[pivotIndex];
         }
-        else if (k < pivotIndex) {
-            return execute(arr, start, pivotIndex - 1, k - start);
+        else if (k < adjustedPivotIndex) {
+//            System.out.println("smaller: k = " + k);
+            return execute(arr, start, pivotIndex - 1, k);
         }
         else {
-            return execute(arr, pivotIndex + 1, end, k - (start + pivotIndex + 1));
+//            System.out.println("larger: k = " + k + " - " + (adjustedPivotIndex + 1));
+            return execute(arr, pivotIndex + 1, end, k - (adjustedPivotIndex + 1));
         }
+    }
+
+    @Override
+    public int getMinSize() {
+        return MIN_SEQUENCE_SIZE;
+    }
+
+    @Override
+    public int getParallelism() {
+        return 1;
+    }
+
+    @Override
+    public void setMinSize(int size) {
+        // no min size - do nothing
+    }
+
+    @Override
+    public void setParallelism(int parallelism) {
+        // sequential - do nothing
+    }
+
+    @Override
+    public String toString() {
+        return "QuickSelect Sequential | Minimum Array Size = " + MIN_SEQUENCE_SIZE;
+    }
+
+    @Override
+    public String toString(boolean minSize, boolean parallelism) {
+        StringBuilder sb = new StringBuilder();
+        sb.append("QuickSelect Sequential ");
+        if (minSize) {
+            sb.append("| Minimum Array Size = " + MIN_SEQUENCE_SIZE);
+        }
+        return sb.toString();
     }
 }
