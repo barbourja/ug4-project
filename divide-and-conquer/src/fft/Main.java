3f1ebf015f59a503f2f045678e0dce975b87bd81
barbourja <s2053424@ed.ac.uk>
2024-02-03 17:33:58 +0000

    Finalized varying parallelism testing for FFT

--- divide-and-conquer/src/fft/Main.java
+++ divide-and-conquer/src/fft/Main.java
@@ -8,14 +8,14 @@ public class Main {
         int inputSize = (int) Math.pow(2, N);
         int seqMinSize = 8;
 
-        Integer[] parallelismValues = new Integer[]{4, 8, 16, 32, 64};
+        Integer[] parallelismValues = new Integer[]{1, 2, 4, 8, 16, 32, 64};
 
         FFTStrategy sequential = new Sequential(seqMinSize);
         FFTStrategy forkJoin = new ForkJoin(1, 1, sequential);
         FFTStrategy threaded = new Threaded(1, 1, sequential);
 
 
-        FFTTestSuite fftTest = new FFTTestSuite(5);
+        FFTTestSuite fftTest = new FFTTestSuite(10);
         fftTest.testVaryingParallelism(sequential, inputSize, parallelismValues,true);
         fftTest.testVaryingParallelism(forkJoin, inputSize,  parallelismValues, true);
         fftTest.testVaryingParallelism(threaded, inputSize,  parallelismValues, true);
